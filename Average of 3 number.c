f16b2edaf211084334523c5f5ee669237b4ac036
Mahfujar Rahman <mahfujarrahmanjoy@gmail.com>
2023-11-26 19:58:02 +0600

    Average of 3 numbers

---
+++ Average of 3 number.c
@@ -0,0 +1,15 @@
+/*
+Average of 3 numbers
+*/
+#include <stdio.h>
+
+int main() {
+
+    int a, b, c, avg;
+    a = 3;
+    b = 4;
+    c = 5;
+    avg = (a+b+c)/3;
+    printf("The average of these 3 numbers are: %d", avg);
+    return 0;
+}
